af8dced6ceb26f803f8f7e55347a6ede11ce2118
Bharath Bhargav <bharathbhargavgb@gmail.com>
2020-11-01 19:26:07 +0530

    Configure app to run at system startup

--- Digital Quarantine/installer/buildPkg.sh
+++ Digital Quarantine/installer/buildPkg.sh
@@ -1,9 +1,26 @@
 # Run this script in the directory containing the .app file
 
-mkdir to_bundle
-mv Digital\ Quarantine.app to_bundle
+app_name="Digital Quarantine.app"
+installer_name="DigitalQuarantineInstaller.pkg"
+bundle_dir="to_bundle"
+scripts_dir="Scripts"
 
-pkgbuild --root to_bundle --identifier com.unnecessary-labs.mac.Digital-Quarantine --install-location /Applications/ DQ.pkg
-productbuild --distribution distribution.xml --resources ./resources/ DigitalQuarantineInstaller.pkg
+if [ -e $installer_name ]
+then
+    rm -rf $installer_name
+fi
+
+if [ -d $bundle_dir ]
+then
+    rm -rf $bundle_dir
+fi
+
+mkdir $bundle_dir
+mv "$app_name" $bundle_dir
+
+chmod 755 $scripts_dir/postinstall
+
+pkgbuild --root $bundle_dir --scripts $scripts_dir --identifier com.unnecessary-labs.mac.Digital-Quarantine --install-location /Applications/ DQ.pkg
+productbuild --distribution distribution.xml --resources ./resources/ $installer_name
 
 rm -rf DQ.pkg
