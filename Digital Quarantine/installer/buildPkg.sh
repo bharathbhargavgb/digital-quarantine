c5aca86cf033abfa93a3a6d9352c1762616fd766
Bharath Bhargav <bharathbhargavgb@gmail.com>
2021-03-20 16:20:44 +0530

    Configure LaunchAgent to update and restart app immediately with future updates
    1. Unload the launchAgent and kill existing process in
       preinstall script of the installer.
    2. Copy the new launchAgent and restart with launchctl command in
       postinstall script of the installer

--- Digital Quarantine/installer/buildPkg.sh
+++ Digital Quarantine/installer/buildPkg.sh
@@ -18,7 +18,7 @@ fi
 mkdir $bundle_dir
 mv "$app_name" $bundle_dir
 
-chmod 755 $scripts_dir/postinstall
+chmod -R 755 $scripts_dir
 
 pkgbuild --root $bundle_dir --scripts $scripts_dir --identifier com.unnecessary-labs.mac.Digital-Quarantine --install-location /Applications/ DQ.pkg
 productbuild --distribution distribution.xml --resources ./resources/ $installer_name
