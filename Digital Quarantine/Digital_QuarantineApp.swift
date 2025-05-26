d24f391d221e717c0fb177a69238288e687f2cf9
Bharath Bhargav <bharathbhargavgb@gmail.com>
2025-05-25 18:19:16 -0700

    Initial Commit

---
+++ Digital Quarantine/Digital_QuarantineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Digital_QuarantineApp.swift
+//  Digital Quarantine
+//
+//  Created by Bharath Bhargav on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Digital_QuarantineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
